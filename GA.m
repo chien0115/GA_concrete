11826913dbe48ca7b8976dcbb9b555db64f1766b
chien0115 <133206218+chien0115@users.noreply.github.com>
2024-09-18 19:31:17 +0800

    GA

--- GA.m
+++ GA.m
@@ -8,25 +8,39 @@ n = 200; % 初始種群大小
 c = 30; % 需要進行交叉的染色體對數
 m = 50; % 需要進行突變的染色體數
 tg = 100; % 總代數
-num_sites = 3; % 工地數
+num_sites = 5; % 工地
+% num_sites_with_factory = num_sites + 1; % 包括工廠的總工地數
 t = 3; % 卡車數
-
-
-% 定義各工地的參數
-start_time = [8*60, 8*60, 8*60+30]; % 各工地開始施工的時間 (以分鐘計算)
-work_time = [20, 30, 25]; % 各工地施工時間 (分鐘)
-travel_time_to = [30, 25, 40]; % 去程時間 (分鐘)
-travel_time_back = [25, 20, 30]; % 回程時間 (分鐘)
-max_interrupt_time = [5, 5, 15]; % 工地最大容許中斷時間 (分鐘)
-max_truck__interrupt_time=[3, 4, 3];%預拌車最大容許中斷時間 (分鐘)
-demand_trips = [2, 2, 1]; % 各工地需求車次
-
-penalty_site_value = 5; % 懲罰時間 (分鐘)
-penalty_truck_value = 3;
-
-
 crossoverRate = 0.8; % 定義交配率
 mutationRate = 0.1; % 突變率
+
+
+
+time_windows = [480, 1020; 490, 1020; 485, 1020; 495, 1020; 490, 1020;0, 1440]; % 每個工地的時間窗(每個工地每天開始、結束時間),0-1440代表工廠全天開放
+% route = [1, 2, 3, 4, 5]; % 染色體示例：工廠到工地的路徑
+
+time = [
+    30, 25;  % 去程到工地 1 需要 30 分鐘，回程需要 25 分鐘
+    25, 20;  % 去程到工地 2 需要 25 分鐘，回程需要 20 分鐘
+    40, 30;  % 去程到工地 3 需要 40 分鐘，回程需要 30 分鐘
+    35, 30;  % 去程到工地 4 需要 35 分鐘，回程需要 30 分鐘
+    20, 15;  % 去程到工地 5 需要 20 分鐘，回程需要 15 分鐘
+];
+
+% 定義各工地的參數
+max_interrupt_time = [5, 5, 15,15,10]; % 工地最大容許中斷時間 (分鐘)
+truck_max_interrupt_time = 25;
+work_time = [20, 30, 25,20,15]; % 各工地施工時間 (分鐘)
+demand_trips = [2, 2, 1, 3, 2]; % 各工地需求車次
+penalty_rate_per_min = 30;% 每分鐘延遲的罰金
+
+
+% start_time = [8*60, 8*60, 8*60+30]; % 各工地開始施工的時間 (以分鐘計算)
+% travel_time_to = [30, 25, 40]; % 去程時間 (分鐘)
+% travel_time_back = [25, 20, 30]; % 回程時間 (分鐘)
+% penalty_site_value = 5; % 懲罰時間 (分鐘)
+
+
 
 
 figure
@@ -39,13 +53,11 @@ hold on
 
 for i = 1:tg
     % 初始化
-    P = population(n, demand_trips); % 初始化種群
+    [P, dispatch_times] = population(n, demand_trips, t, time_windows); % 初始化種群
     K = zeros(tg, 2); % 儲存適應度的矩陣
     [x1, y1] = size(P);
     % 交配操作
-    [E, dispatch_times] = evaluation(P, t, start_time, work_time, travel_time_to, travel_time_back, max_interrupt_time, max_truck__interrupt_time, demand_trips, penalty_site_value,penalty_truck_value); % 評估族群 P 中每個染色體的適應度
     
-    [P, S, best_dispatch_times] = selection(P, E, n,dispatch_times); % 根據適應度值 E 選擇族群中的染色體
 
     if rand() <= crossoverRate
         % 選擇交叉的染色體對
@@ -62,7 +74,9 @@ for i = 1:tg
     end
 
     % 評估操作
+    [E, dispatch_times] = evaluation(P, t, time_windows, dispatch_times, work_time, time, max_interrupt_time, truck_max_interrupt_time, demand_trips, penalty_rate_per_min); % 評估族群 P 中每個染色體的適應度
     
+    [P, S, best_dispatch_times] = selection(P, E, n,dispatch_times); % 根據適應度值 E 選擇族群中的染色體
     
 
     K(i,1) = sum(S) / n; % 平均適應度

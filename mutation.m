d80ea699dc636aa9f523083d750e0c4a74f09df0
chien0115 <133206218+chien0115@users.noreply.github.com>
2024-09-19 03:16:42 +0800

    mutation

--- mutation.m
+++ mutation.m
@@ -1,31 +1,37 @@
-function Y = mutation(P, n)
-[x1, y1] = size(P); % 獲取 P 的尺寸 (染色體數量和基因數量)
-Z = zeros(n, y1); % 初始化一個 n 行 y1 列的矩陣 Z，用來存儲變異後的新染色體
+function [Y, dispatch_times_new] = mutation(P, t, dispatch_times, n)
+% P = Population
+% dispatch_times = Matrix of dispatch times corresponding to the chromosomes
+% n = Number of mutations to perform
+
+[x1, y1] = size(P); % Size of the population and chromosomes
+Z = zeros(n, y1); % Initialize matrix to store new chromosomes
+dispatch_times_new = zeros(n, size(dispatch_times, 2)); % Initialize matrix to store new dispatch times
+
+% Define the range for the positions to be swapped (only in scheduling part)
+num_scheduling_positions = y1 - size(dispatch_times, 2); % Number of scheduling positions
+odd_positions = 1:2:num_scheduling_positions; % Get all odd positions in the scheduling part
 
 for i = 1:n
-    % 隨機選擇一條染色體
-    r1 = randi(x1);
+    % Randomly select a chromosome 
+    r1 = randi(x1); % Randomly select an index within the range of population size
     A1 = P(r1, :);
+    dispatch_times1 = dispatch_times(r1, :);
 
-    % 隨機選擇兩個不同的位置進行交換
-    pos = randperm(y1, 2);
-    % 交換兩個位置的值
+    % Ensure only odd positions are used for swapping
+    pos = randperm(length(odd_positions), 2); % Randomly select two different odd positions
+    pos = odd_positions(pos); % Get the actual positions
+
+    % Swap the values at the selected positions (dispatch order adjustment)
     A1([pos(1), pos(2)]) = A1([pos(2), pos(1)]);
 
-    % 確保變異後的染色體中不包含0
-    % while any(A1 == 0)
-    %     % 如果染色體中包含0，重新選擇一條染色體進行變異
-    %     r1 = randi(x1);
-    %     A1 = P(r1, :);
-    % 
-    %     % 隨機選擇兩個不同的位置進行交換
-    %     pos = randperm(y1, 2);
-    %     % 交換兩個位置的值
-    %     A1([pos(1), pos(2)]) = A1([pos(2), pos(1)]);
-    % end
+    % Swap dispatch times at the same positions
+    pos_dispatch = randperm(size(dispatch_times1, 2), 2); % Randomly select two different positions within dispatch_times1
+    dispatch_times1([pos_dispatch(1), pos_dispatch(2)]) = dispatch_times1([pos_dispatch(2), pos_dispatch(1)]);
 
-    % 將變異後的染色體存儲到 Z 中
+    % Store the new chromosome and dispatch times
     Z(i, :) = A1;
+    dispatch_times_new(i, :) = dispatch_times1;
 end
-Y = Z; % 返回變異後的族群
+
+Y = Z; % Return the new population
 end

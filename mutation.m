01e2bbc7fc0f524e6bc390554a64aed75a2b05ad
chien0115 <133206218+chien0115@users.noreply.github.com>
2024-09-20 01:23:15 +0800

    mutation

--- mutation.m
+++ mutation.m
@@ -1,20 +1,22 @@
-function [Y, dispatch_times_new2] = mutation(P, t, dispatch_times, n)
+function [Y, dispatch_times_new2] = mutation(P, t, dispatch_times, m)
 % P = Population
 % dispatch_times = Matrix of dispatch times corresponding to the chromosomes
 % n = Number of mutations to perform
 
-[x1, y1] = size(P); % Size of the population and chromosomes
-Z = zeros(n, y1); % Initialize matrix to store new chromosomes
-dispatch_times_new2 = zeros(n, size(dispatch_times, 2)); % Initialize matrix to store new dispatch times
-
+[x1, y1] = size(P); % Size of the population and chromosomes 400,23
+Z = zeros(m, y1); % Initialize matrix to store new chromosomes m,23
+dispatch_times_new2 = zeros(m, size(dispatch_times, 2)); % Initialize matrix to store new dispatch times
 % Define the range for the positions to be swapped (only in scheduling part)
 num_scheduling_positions = y1 - size(dispatch_times, 2); % Number of scheduling positions
 odd_positions = 1:2:num_scheduling_positions; % Get all odd positions in the scheduling part
+disp(['Size of dispatch_times: ', num2str(size(dispatch_times, 1)), ' x ', num2str(size(dispatch_times, 2))]);
 
-for i = 1:n
-    % Randomly select a chromosome 
+for i = 1:m %目前x1=400 y1=23 n=5
+    % Randomly select a chromosome
+    disp(['x1: ', num2str(x1)]);
     r1 = randi(x1); % Randomly select an index within the range of population size
-    A1 = P(r1(1), 1:y1-t);%只有派遣順序
+    disp(['Selected r1: ', num2str(r1)]);
+    A1 = P(r1, 1:y1-t);%只有派遣順序
     dispatch_times1 = dispatch_times(r1, :);
 
     % Ensure only odd positions are used for swapping
